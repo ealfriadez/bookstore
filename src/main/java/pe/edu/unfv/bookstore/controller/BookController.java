b1c93da44f87b9a7f3744801f0b215457eb6c86a
Eleazar Alfredo, ALFRIADEZ YRIARTE <ealfriadez@gmail.com>
2026-06-13 10:42:51 -0500

    Finalizando primera parte

--- src/main/java/pe/edu/unfv/bookstore/controller/BookController.java
+++ src/main/java/pe/edu/unfv/bookstore/controller/BookController.java
@@ -13,10 +13,16 @@ import java.util.List;
 @RequestMapping("/api/books")
 public class BookController {
 
-   private List<Book> books = new ArrayList<>(List.of(
+   /*private List<Book> books = new ArrayList<>(List.of(
             new Book(1L, "Clean Code", "Robert C. Martin", 45.00),
             new Book(2L, "The Pragmatic", "Andy Hunt", 50.00),
             new Book(3L, "Refactoring", "Martin Fowler", 55.00)
+    ));*/
+
+    private List<Book> books = new ArrayList<>(List.of(
+            Book.builder().id(1L).title("Clean Code").author("Robert C. Martin").price(45.00).build(),
+            Book.builder().id(2L).title("The Pragmatic").author("Andy Hunt").price(50.00).build(),
+            Book.builder().id(3L).title("Refactoring").author("Martin Fowler").price(55.00).build()
     ));
 
    private Long contadorId = 4L;
@@ -27,12 +33,62 @@ public class BookController {
         return new ResponseEntity<>(books, HttpStatus.OK);
     }
 
-    //GET /api/books/{id} -> Buscar por ID
+    //GET /api/books/{id} -> Buscar por ID (@PathVariable)
     @GetMapping("/{id}")
     public ResponseEntity<Book> getById(@PathVariable Long id){
         for (Book book : books) {
             if (book.getId().equals(id)) {
                 return new ResponseEntity<>(book, HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    //GET /api/books/search?author=Martin -> Buscar por autor (@RequestParam)
+    @GetMapping("/search")
+    public ResponseEntity<List<Book>> searchByAuthor(
+            @RequestParam String author
+    ){
+        List<Book> booksByAuthor = new ArrayList<>();
+        for (Book book : books) {
+            if (book.getAuthor().contains(author)) {
+                booksByAuthor.add(book);
+            }
+        }
+        return new ResponseEntity<>(booksByAuthor, HttpStatus.OK);
+    }
+
+    //POST /api/books -> Crear un nuevo libro (@RequestBody)
+    @PostMapping
+    public ResponseEntity<Book> save(@RequestBody Book book) {
+        book.setId(contadorId++);
+        books.add(book);
+        return new ResponseEntity<>(book, HttpStatus.CREATED);
+    }
+
+    //PUT /api/books -> Crear un nuevo libro (@RequestBody)
+    @PutMapping("/{id}")
+    public ResponseEntity<Book> update(
+            @PathVariable Long id,
+            @RequestBody Book book) {
+        for (Book b : books){
+            if(b.getId().equals(id)){
+                b.setTitle(book.getTitle());
+                b.setAuthor(book.getAuthor());
+                b.setPrice(book.getPrice());
+                return new ResponseEntity<>(b, HttpStatus.OK);
+            }
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    //DELETE /api/books/{id} -> Eliminar un libro por ID (@PathVariable)
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Book> delete(@PathVariable Long id) {
+        for (Book book : books) {
+            if (book.getId().equals(id)) {
+                books.remove(book);
+                return new ResponseEntity<>(HttpStatus.NO_CONTENT);
             }
         }
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
--- src/main/java/pe/edu/unfv/bookstore/model/Book.java
+++ src/main/java/pe/edu/unfv/bookstore/model/Book.java
@@ -1,50 +1,15 @@
 package pe.edu.unfv.bookstore.model;
 
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Book {
     private Long id;
     private String  title;
     private String author;
     private double price;
-
-    public Book() {
-    }
-
-    public Book(Long id, String title, String author, double price) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.price = price;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getAuthor() {
-        return author;
-    }
-
-    public void setAuthor(String author) {
-        this.author = author;
-    }
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
 }
